de1c6ba0c0611655b1e7dffd57a4329fdb2a0062
YevhenBez <byxgalter@bigmir.net>
2023-05-26 14:44:10 +0300

    Переписал Модалку на хуки. Задание 3 Модуль 4   REACT
    Переписал Модалку на хуки.  Для этого импортнул   useEffect    Переписал компонент Modal    на обычную функцию, в которой сразу деструктуризировал переданные ему Пропсы при помощи    { onClose, image }     Для установки, снятия Слушателя, а также для закрытия Модалки при помощи кнопки    Escape   использовал    useEffect(() => { })   Также поубирал   this    Закомментированный код - это как было реализовано тоже самое на Классах.   Задание 3 Модуль 4   REACT

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,50 +1,93 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import css from './css/modal.module.css';
 import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modalRoot');
 
-class Modal extends React.Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.keyDown);
-  }
+const Modal = ({ onClose, image }) => {
+  useEffect(() => {
+    const keyDown = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.keyDown);
-  }
+    window.addEventListener('keydown', keyDown);
 
-  keyDown = event => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+    return () => {
+      window.removeEventListener('keydown', keyDown);
+    };
+  }, [onClose]);
 
-  onOverlayClose = event => {
+  const onOverlayClose = event => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const { largeImageURL, tags } = this.props.image;
-    return createPortal(
-      <div onClick={this.onOverlayClose} className={css.overlay}>
-        <div className={css.modal}>
-          <img src={largeImageURL} alt={tags} />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  const { largeImageURL, tags } = image;
+
+  return createPortal(
+    <div onClick={onOverlayClose} className={css.overlay}>
+      <div className={css.modal}>
+        <img src={largeImageURL} alt={tags} />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   image: PropTypes.shape({
     largeImageURL: PropTypes.string.isRequired,
     tags: PropTypes.string.isRequired,
-    }),
+  }),
   onClose: PropTypes.func.isRequired,
 };
 
 export default Modal;
+
+// class Modal extends React.Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.keyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.keyDown);
+//   }
+
+//   keyDown = event => {
+//     if (event.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   onOverlayClose = event => {
+//     if (event.currentTarget === event.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const { largeImageURL, tags } = this.props.image;
+//     return createPortal(
+//       <div onClick={this.onOverlayClose} className={css.overlay}>
+//         <div className={css.modal}>
+//           <img src={largeImageURL} alt={tags} />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
+
+// Modal.propTypes = {
+//   image: PropTypes.shape({
+//     largeImageURL: PropTypes.string.isRequired,
+//     tags: PropTypes.string.isRequired,
+//     }),
+//   onClose: PropTypes.func.isRequired,
+// };
+
+// export default Modal;
