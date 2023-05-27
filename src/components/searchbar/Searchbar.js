36eed3d7ba22a9443fb7fa4a0a852adf34ee0d16
YevhenBez <byxgalter@bigmir.net>
2023-05-27 15:03:43 +0300

    Переделал на хуки. Задание 3 Модуль 4   REACT
    Переделал на хуки. Для этого импортнул  useState   Компонент    Searchbar    переделал на обычную функцию деструктуризировав в ней сразу принятый Пропс при помощи  { onSubmit }   Также в данной функции задал начальное значение   inputData    при помощи   const [inputData, setInputData] = useState('');  Также переделал под использование Хуков функцию      onChangeInput     и   handleSubmit    Также поубирал   this     Закомментированный код - это как было реализовано тоже самое на Классе. Задание 3 Модуль 4   REACT

--- src/components/searchbar/Searchbar.js
+++ src/components/searchbar/Searchbar.js
@@ -1,49 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from './css/searchbar.module.css';
 import PropTypes from 'prop-types';
 
-class Searchbar extends React.Component {
-  state = {
-    inputData: '',
+const Searchbar = ({ onSubmit }) => {
+  const [inputData, setInputData] = useState('');
+
+  const onChangeInput = event => {
+    setInputData(event.currentTarget.value.toLowerCase());
   };
 
-  onChangeInput = event => {
-    this.setState({ inputData: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.onSubmit(this.state.inputData);
-    this.setState({ inputData: '' });
+    onSubmit(inputData);
+    setInputData('');
   };
 
-  render() {
-    const { inputData } = this.state.inputData;
-    return (
-      <header className={css.searchbar}>
-        <form className="form" onSubmit={this.handleSubmit}>
-          <button type="submit" className="button">
-            <span className="button-label">Search</span>
-          </button>
+  return (
+    <header className={css.searchbar}>
+      <form className="form" onSubmit={handleSubmit}>
+        <button type="submit" className="button">
+          <span className="button-label">Search</span>
+        </button>
 
-          <input
-            className="input"
-            name="inputData"
-            value={inputData}
-            onChange={this.onChangeInput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className="input"
+          name="inputData"
+          value={inputData}
+          onChange={onChangeInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
 
 Searchbar.propType = {
   onSubmit: PropTypes.func.isRequired,
 };
 
 export default Searchbar;
+
+// class Searchbar extends React.Component {
+//   state = {
+//     inputData: '',
+//   };
+
+//   onChangeInput = event => {
+//     this.setState({ inputData: event.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     this.props.onSubmit(this.state.inputData);
+//     this.setState({ inputData: '' });
+//   };
+
+//   render() {
+//     const { inputData } = this.state.inputData;
+//     return (
+//       <header className={css.searchbar}>
+//         <form className="form" onSubmit={this.handleSubmit}>
+//           <button type="submit" className="button">
+//             <span className="button-label">Search</span>
+//           </button>
+
+//           <input
+//             className="input"
+//             name="inputData"
+//             value={inputData}
+//             onChange={this.onChangeInput}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
+
+// Searchbar.propType = {
+//   onSubmit: PropTypes.func.isRequired,
+// };
+
+// export default Searchbar;
