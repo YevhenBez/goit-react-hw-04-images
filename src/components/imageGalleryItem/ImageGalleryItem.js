3e2e83fd570b054b1b9b0836f7da955fbdb4faf3
YevhenBez <byxgalter@bigmir.net>
2023-05-27 14:12:46 +0300

    Переделал на хуки Задание 3 Модуля 4   REACT
    Переделал на хуки. Для этого импортнул   useState   Компонент   ImageGalleryItem   переделал на обычную функцию и сразу в ней деструктуризировал принятый ей Пропс при помощи    { item }   Также в данной функции  задал начальное значение   shownModal    при помощи   const [shownModal, setShownModal] = useState(false);    Функцию    onModal      пришлось изменить, она только открывает Модальное окно. Для закрытия  Модального окна пришлось написать функцию   const closeModal = () => {
        setShownModal(false);
      };  Также поубирал    this    и в тег Модалки поставил функцию closeModal  для закрытия Модального окна, вот таким макаром     {shownModal && <Modal onClose={closeModal} image={item} />}    Закомментированный код - это как было реализовано тоже самое на Классе.  Задание  3 Модуля 4   REACT

--- src/components/imageGalleryItem/ImageGalleryItem.js
+++ src/components/imageGalleryItem/ImageGalleryItem.js
@@ -1,36 +1,71 @@
 import Modal from '../modal/Modal';
-import React from 'react';
+import React, { useState } from 'react';
 import css from './css/imageGalleryItem.module.css';
 import PropTypes from 'prop-types';
 
-class ImageGalleryItem extends React.Component {
-  state = {
-    shownModal: false,
+const ImageGalleryItem = ({ item }) => {
+  const [shownModal, setShownModal] = useState(false);
+
+  const onModal = () => {
+    setShownModal(true);
   };
-  onModal = () => {
-    this.setState(({ shownModal }) => ({ shownModal: !shownModal }));
+
+  const closeModal = () => {
+    setShownModal(false);
   };
-  render() {
-    const { item } = this.props;
-    const { webformatURL, tags } = item;
-    return (
-      <li className={css.imageGalleryItem}>
-        <img
-          onClick={this.onModal}
-          className={css.imageGalleryItemPicture}
-          src={webformatURL}
-          alt={tags}
-        />
-        {this.state.shownModal && <Modal onClose={this.onModal} image={item} />}
-      </li>
-    );
-  }
-}
+
+  const { webformatURL, tags } = item;
+
+  return (
+    <li className={css.imageGalleryItem}>
+      <img
+        onClick={onModal}
+        className={css.imageGalleryItemPicture}
+        src={webformatURL}
+        alt={tags}
+      />
+      {shownModal && <Modal onClose={closeModal} image={item} />}
+    </li>
+  );
+};
 
 ImageGalleryItem.propTypes = {
   item: PropTypes.shape({
     webformatURL: PropTypes.string.isRequired,
     tags: PropTypes.string.isRequired,
-    }),}; 
+  }),
+};
 
 export default ImageGalleryItem;
+
+// class ImageGalleryItem extends React.Component {
+//   state = {
+//     shownModal: false,
+//   };
+//   onModal = () => {
+//     this.setState(({ shownModal }) => ({ shownModal: !shownModal }));
+//   };
+//   render() {
+//     const { item } = this.props;
+//     const { webformatURL, tags } = item;
+//     return (
+//       <li className={css.imageGalleryItem}>
+//         <img
+//           onClick={this.onModal}
+//           className={css.imageGalleryItemPicture}
+//           src={webformatURL}
+//           alt={tags}
+//         />
+//         {this.state.shownModal && <Modal onClose={this.onModal} image={item} />}
+//       </li>
+//     );
+//   }
+// }
+
+// ImageGalleryItem.propTypes = {
+//   item: PropTypes.shape({
+//     webformatURL: PropTypes.string.isRequired,
+//     tags: PropTypes.string.isRequired,
+//     }),};
+
+// export default ImageGalleryItem;
