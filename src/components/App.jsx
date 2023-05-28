9025fbce27c93b582033186c417a2fbb0f5c42cb
YevhenBez <byxgalter@bigmir.net>
2023-05-28 13:44:44 +0300

    Переделал на Хуки. Задание 3 Модуль 4    REACT
    Переделал на Хуки. Для этого импортнул    useState,     useEffect    и useRef    Переделал компонент   Арр   на обычную  функцию, чтобы было так  const App = () => { }   и сразу же в ней задал начальные значения для     inputData  и т.п.  при помощи    useState     - код выглядит так   const [inputData, setInputData] = useState('');    и т.п.   Чтобы отслеживать предыдущие значения    inputData    и     page    использовал алгоритм    function usePreviousValue(value) {
        const ref = useRef();
        useEffect(() => {
          ref.current = value;
        });
        return ref.current;
      }         const previousInputData = usePreviousValue(inputData);
      const previousPage = usePreviousValue(page);      описанный  на ресурсе    https://dev.to/mcavaliere/comparing-previous-useeffect-values-in-react-2o4g     Затем всю логику, написанную на Классе раньше  в   componentDidUpdate   , я обернул  в     useEffect(() => { ), [inputData, previousInputData, previousPage, page]);   В котором причесал немного условия выполнения кода, используя    previousInputData    и    previousPage    описанные выше. Для использования   async  в условиях кода, где был   await   создал функцию   getImages();     а затем обернул ею код с   try    и   catch   в котором был   await    используя конструкцию    async function getImages() { }    Также поудалял    this     , а для изменения значений использовал    setStatus и т.п. вещи Хуков. Также переделал под примочки Хуков функцию   handleSubmit    и    onNextPage     Не нужное, в связи с переделыванием на Хуки удалил, а закомментированный код - это как было реализовано тоже самое на Классе. Задание 3 Модуль 4    REACT

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Searchbar from './searchbar/Searchbar';
 import Button from './button/Button';
 import ImageGallery from './ImageGallery/ImageGallery';
@@ -6,110 +6,222 @@ import { fetchImages } from '../fetchImages';
 import Loader from './loader/Loader';
 import css from './app.module.css';
 
+const App = () => {
+  const [inputData, setInputData] = useState('');
+  const [items, setItems] = useState([]);
+  const [page, setPage] = useState(1);
+  const [status, setStatus] = useState('idle');
+  const [totalHits, setTotalHits] = useState(0);
+  const [showButtonMore, setShowButtonMore] = useState(false);
 
-class App extends React.Component {
-  state = {
-    inputData: '',
-    items: [],
-    page: 1, 
-    status: 'idle',
-    totalHits: 0,
-    showButtonMore: false
-  };
+  function usePreviousValue(value) {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  }
 
-  async componentDidUpdate(_, prevState) {
-    const { page, inputData } = this.state;
+  const previousInputData = usePreviousValue(inputData);
+  const previousPage = usePreviousValue(page);
+
+  useEffect(() => {
     if (inputData.trim() === '') {
       return;
     }
-    if (prevState.inputData !== inputData) {
-      try {
-        this.setState({ status: 'pending' });
-        const { totalHits, hits } = await fetchImages(inputData, page);
-        if (hits.length < 1) {
-          this.setState({ status: 'idle' });
-        } else {
-          this.setState({
-            items: hits,
-            inputData,
-            totalHits: totalHits,
-            status: 'resolved',
-            showButtonMore: true
-          });
+    if (previousInputData !== inputData) {
+      getImages();
+      async function getImages() {
+        try {
+          setStatus('pending');
+          const { totalHits, hits } = await fetchImages(inputData, page);
+          if (hits.length < 1) {
+            setStatus('idle');
+          } else {
+            setItems(hits);
+            setInputData(inputData);
+            setTotalHits(totalHits);
+            setStatus('resolved');
+            setShowButtonMore(true);
+          }
+        } catch (error) {
+          setStatus('rejected');
         }
-      } catch (error) {
-        this.setState({ status: 'rejected' });
       }
     }
-    if (prevState.page !== page && prevState.inputData === inputData) {
-      this.setState({ status: 'pending' });
+    if (previousPage !== page && previousInputData === inputData) {
+      getImages();
+      async function getImages() {
+        setStatus('pending');
+        try {
+          const { hits } = await fetchImages(inputData, page);
 
-      try {
-        const { hits } = await fetchImages(inputData, page);
-        this.setState(prevState => ({
-          items: [...prevState.items, ...hits],
-          status: 'resolved',
-          showButtonMore: true
-        }));
-      } catch (error) {
-        this.setState({ status: 'rejected' });
+          setItems(prevItems => [...prevItems, ...hits]);
+          setStatus('resolved');
+          setShowButtonMore(true);
+        } catch (error) {
+          setStatus('rejected');
+        }
       }
     }
-  }
+  }, [inputData, previousInputData, previousPage, page]);
 
-  handleSubmit = inputData => {
-    this.setState({
-      inputData,
-      items: [],
-      page: 1,
-      status: 'idle',
-      totalHits: 0,
-    });
+  const handleSubmit = inputData => {
+    setInputData(inputData);
+    setItems([]);
+    setPage(1);
+    setStatus('idle');
+    setTotalHits(0);
   };
 
-  onNextPage = () => {
-    this.setState(prevState => ({ page: prevState.page + 1, showButtonMore: false }));
+  const onNextPage = () => {
+    setPage(prevPage => prevPage + 1);
+    setShowButtonMore(false);
   };
 
-  render() {
-    const { totalHits, status, items, showButtonMore } = this.state;
-    if (status === 'idle') {
-      return (
-        <div className={css.app}>
-          <Searchbar onSubmit={this.handleSubmit} />
-        </div>
-      );
-    }
-    if (status === 'pending') {
-      return (
-        <div className={css.app}>
-          <Searchbar onSubmit={this.handleSubmit} />
-          <ImageGallery items={this.state.items} />
-          <Loader />
-          {showButtonMore && totalHits > 12 && <Button onClick={this.onNextPage} />}
-        </div>
-      );
-    }
-    if (status === 'rejected') {
-      return (
-        <div className={css.app}>
-          <Searchbar onSubmit={this.handleSubmit} />
-          <p>Something wrong, try later</p>
-        </div>
-      );
-    }
-    if (status === 'resolved') {
-      return (
-        <div className={css.app}>
-          <Searchbar onSubmit={this.handleSubmit} />
-          <ImageGallery items={this.state.items} />
-          {showButtonMore && totalHits > 12 && totalHits > items.length && (
-            <Button onClick={this.onNextPage} />
-          )}
-        </div>
-      );
-    }
+  if (status === 'idle') {
+    return (
+      <div className={css.app}>
+        <Searchbar onSubmit={handleSubmit} />
+      </div>
+    );
   }
-}
+  if (status === 'pending') {
+    return (
+      <div className={css.app}>
+        <Searchbar onSubmit={handleSubmit} />
+        <ImageGallery items={items} />
+        <Loader />
+        {showButtonMore && totalHits > 12 && <Button onClick={onNextPage} />}
+      </div>
+    );
+  }
+  if (status === 'rejected') {
+    return (
+      <div className={css.app}>
+        <Searchbar onSubmit={handleSubmit} />
+        <p>Something wrong, try later</p>
+      </div>
+    );
+  }
+  if (status === 'resolved') {
+    return (
+      <div className={css.app}>
+        <Searchbar onSubmit={handleSubmit} />
+        <ImageGallery items={items} />
+        {showButtonMore && totalHits > 12 && totalHits > items.length && (
+          <Button onClick={onNextPage} />
+        )}
+      </div>
+    );
+  }
+};
 
 export default App;
+
+// class App extends React.Component {
+//   state = {
+//     inputData: '',
+//     items: [],
+//     page: 1,
+//     status: 'idle',
+//     totalHits: 0,
+//     showButtonMore: false
+//   };
+
+//   async componentDidUpdate(_, prevState) {
+//     const { page, inputData } = this.state;
+//     if (inputData.trim() === '') {
+//       return;
+//     }
+//     if (prevState.inputData !== inputData) {
+//       try {
+//         this.setState({ status: 'pending' });
+//         const { totalHits, hits } = await fetchImages(inputData, page);
+//         if (hits.length < 1) {
+//           this.setState({ status: 'idle' });
+//         } else {
+//           this.setState({
+//             items: hits,
+//             inputData,
+//             totalHits: totalHits,
+//             status: 'resolved',
+//             showButtonMore: true
+//           });
+//         }
+//       } catch (error) {
+//         this.setState({ status: 'rejected' });
+//       }
+//     }
+//     if (prevState.page !== page && prevState.inputData === inputData) {
+//       this.setState({ status: 'pending' });
+
+//       try {
+//         const { hits } = await fetchImages(inputData, page);
+//         this.setState(prevState => ({
+//           items: [...prevState.items, ...hits],
+//           status: 'resolved',
+//           showButtonMore: true
+//         }));
+//       } catch (error) {
+//         this.setState({ status: 'rejected' });
+//       }
+//     }
+//   }
+
+//   handleSubmit = inputData => {
+//     this.setState({
+//       inputData,
+//       items: [],
+//       page: 1,
+//       status: 'idle',
+//       totalHits: 0,
+//     });
+//   };
+
+//   onNextPage = () => {
+//     this.setState(prevState => ({ page: prevState.page + 1, showButtonMore: false }));
+//   };
+
+//   render() {
+//     const { totalHits, status, items, showButtonMore } = this.state;
+//     if (status === 'idle') {
+//       return (
+//         <div className={css.app}>
+//           <Searchbar onSubmit={this.handleSubmit} />
+//         </div>
+//       );
+//     }
+//     if (status === 'pending') {
+//       return (
+//         <div className={css.app}>
+//           <Searchbar onSubmit={this.handleSubmit} />
+//           <ImageGallery items={this.state.items} />
+//           <Loader />
+//           {showButtonMore && totalHits > 12 && <Button onClick={this.onNextPage} />}
+//         </div>
+//       );
+//     }
+//     if (status === 'rejected') {
+//       return (
+//         <div className={css.app}>
+//           <Searchbar onSubmit={this.handleSubmit} />
+//           <p>Something wrong, try later</p>
+//         </div>
+//       );
+//     }
+//     if (status === 'resolved') {
+//       return (
+//         <div className={css.app}>
+//           <Searchbar onSubmit={this.handleSubmit} />
+//           <ImageGallery items={this.state.items} />
+//           {showButtonMore && totalHits > 12 && totalHits > items.length && (
+//             <Button onClick={this.onNextPage} />
+//           )}
+//         </div>
+//       );
+//     }
+//   }
+// }
+
+// export default App;
